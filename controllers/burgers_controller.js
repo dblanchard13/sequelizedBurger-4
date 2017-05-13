7aadaca0cf001ee59f4874e8813a63677661435c
Ricky Grams <rickygrams@Rickys-MacBook-Pro.local>
2017-05-12 22:43:03 -0700

    jaws config

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,52 +1,40 @@
-// Inside the burgers_controller.js file, import the following:
-// Express
-// burger.js
-// Create the router for the app, and export the router at the end of your file.
 
-var express = require("express");
+// Requiring our Burger model
+var db = require("../models");
 
-var router = express.Router();
+// Routes
+// =============================================================
+module.exports = function(app) {
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+    // Create all our routes and set up logic within those routes where required.
+    app.get("/", function(req, res) {
+      db.Burger.findAll({}).then(function(results) {
+        var hbsObject = {
+          burgers: results
+        };
+        res.render("index", hbsObject);
+      });
+    });
+      
+    app.post("/create", function(req, res) {
+      db.Burger.create({
+        burger_name: req.body.burger_name
+      }).then(function(results) {
+        res.redirect("/");
+      });
+    });
+        
+    app.put("/:id", function(req, res) {
+     
+      db.Burger.update({
+        devoured: req.body.devoured 
+        }, {
+          where: {
+            id: req.params.id
+          }
+      }).then(function(results) {
+        res.redirect("/");
+      });
+    });
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    // console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
-
-router.post("/create", function(req, res) {
-  burger.create([
-    "burger_name"
-  ], [
-    req.body.burger_name,
-  ], function() {
-    res.redirect("/");
-  });
-});
-
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  // console.log(req.params.id);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
-});
-
-// Export routes for server.js to use.
-module.exports = router;
-
-
-
-
-
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var exphbs = require("express-handlebars");
 
 // Sets up our express server
 var app = express();
@@ -14,9 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
+// handlebars setup
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -24,15 +23,18 @@ app.set("view engine", "handlebars");
 app.use(methodOverride("_method"));
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static("./public"));
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+require("./controllers/burgers_controller.js")(app);
 
-app.use("/", routes);
-
-
-// Listener function
-app.listen(PORT, function() {
-	console.log("App listening on PORT " + PORT);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
+
